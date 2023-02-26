ccca1b4049388a11715d02741e37849b3949265f
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-02-26 14:06:14 +0100

    task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,10 @@
+
+// Napisz skrypt, który dla każdego elementu tablicy ingredients:
+
+// Utworzy oddzielny element <li>. Koniecznie użyj metody document.createElement().
+// Doda nazwę elementu jako jego zawartość tekstową.
+// Doda do elementu klasę item.
+// Po czym umieści wszystkie <li> na liście podczas jednej operacji ul#ingredients.
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +13,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Wybierz ul#ingredients
+const ul = document.querySelector("#ingredients");
+
+// Iteruj przez każdy element w tablicy ingredients
+ingredients.forEach((ingredient) => {
+  // Utwórz nowy element <li>
+  const li = document.createElement("li");
+
+  // Dodaj klasę "item" do elementu <li>
+  li.classList.add("item");
+
+  // Dodaj tekst składnika do elementu <li>
+  li.textContent = ingredient;
+
+  // Dodaj element <li> do listy <ul>
+  ul.appendChild(li);
+});
