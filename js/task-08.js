4172d1d9ffdc70fcbe3ea3ad5d2d87c246080fe4
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2023-02-26 17:11:04 +0100

    task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,4 +1,9 @@
-
+// Napisz skrypt zarządzania formularzem logowania.
+// // Opracowanie przesyłania formularza form.login-form powinno przebiegać zgodnie ze zdarzeniem submit.
+// Podczas przesyłania formularza strona nie powinna się restartować.
+// Jeśli w formularzu są nieuzupełnione pola, wprowadź alert z upomnieniem o tym, że wszystkie pola powinny zostać wypełnione.
+// Jeśli użytkownik uzupełnił wszystkie pola i wysłał formularz, zbierz wartość pól jako obiekt, gdzie nazwa pola będzie nazwą właściwości, a wartość pola - wartością właściwości. Aby otrzymać dostęp do elementów formularza użyj właściwości elements.
+// Umieść obiekt z wprowadzonymi danymi do wiersza poleceń i wyczyść wartości pól formularza metodą reset
 const loginForm = document.querySelector('form.login-form');
 
 loginForm.addEventListener('submit', (event) => {
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,25 @@
+// Napisz skrypt, który zmienia kolor tła elementu <body> poprzez style inline po kliknięciu na button.change-color i wprowadza wartość koloru do span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Aby wygenerować losowy kolor użyj funkcji getRandomHexColor.
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('.change-color');
+    const colorSpan = document.querySelector('.color');
+
+    button.addEventListener('click', () => {
+      const newColor = getRandomHexColor();
+      document.body.style.backgroundColor = newColor;
+      colorSpan.textContent = newColor;
+    });
+
+    // Skrypt pobiera referencje do przycisku i elementu span, a następnie dodaje do przycisku nasłuchiwacz zdarzeń na kliknięcie. Po kliknięciu generowany jest losowy kolor przy pomocy funkcji getRandomHexColor(), ustawiany jako kolor tła elementu <body> i wyświetlany w elemencie span.
